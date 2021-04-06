8d24cbff12433690fb131fc8e3399003a9b466ed
Reza <hajipour.mrh@gmail.com>
2021-04-06 13:13:08 +0200

    new signup, login, reset password

--- src/components/HeroText.js
+++ src/components/HeroText.js
@@ -1,6 +1,7 @@
 import { Button, Typography, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+
 const useStyle = makeStyles((theme) => ({
   heroButton: {
     width: '9rem',
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import theme from './theme';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
+
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
