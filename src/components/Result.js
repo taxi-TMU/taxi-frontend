5ea9f3d8465549c648566518a726ef956d5ad5e9
Massimiliano Rizzuto <massimilianok@ubuntuk.localdomain>
2021-04-15 17:18:09 +0200

    Refactor result page

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-// import { useLocation } from 'react-router-dom';
-import { makeStyles } from "@material-ui/core/styles";
-import { Link as RouterLink, useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import moment from 'moment';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link as RouterLink, useParams } from 'react-router-dom';
 import {
   Link,
   Typography,
@@ -12,25 +12,34 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
-} from "@material-ui/core";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { AccessTime, ErrorOutline, StarOutline } from "@material-ui/icons";
-import { getRequest } from "../utils/api";
+  Box,
+  CircularProgress,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {
+  AccessTime,
+  CheckCircle,
+  CheckCircleOutlineOutlined,
+  Error,
+  FiberManualRecord,
+} from '@material-ui/icons';
+import { getRequest } from '../utils/api';
 
-const SelectCategory = () => {
-  // const { state } = useLocation();
+const Result = () => {
   const classes = useStyles();
   const { id } = useParams();
   const [loading, setLoading] = useState();
   const [result, setResults] = useState();
-  const [nrQuestions, setNrQuestions] = useState(0)
-  const [wrongAnswers, setWrongAnswers] = useState(0)
+  const [rightAnswers, setRightAnswers] = useState(0);
+  const [time, setTime] = useState(0);
 
   useEffect(() => {
+    setLoading(true);
     const getData = async () => {
-      setLoading(true);
       try {
         const res = await getRequest(`training/${id}`);
+        countAnswers(res);
+        countTime(res);
         setResults(res);
         setLoading(false);
       } catch (err) {
@@ -40,136 +49,196 @@ const SelectCategory = () => {
     getData();
   }, [id]);
 
+  const countAnswers = (result) => {
+    console.log(result);
+    result.questions.forEach((res) => {
+      if (res.answeresRight) setRightAnswers((prevState) => prevState + 1);
+    });
+  };
 
-  // TODO useEffect(() => {
-  //     result && setNrQuestions(result.questions.length)
-  //     result && result.questions.map(question => {
-  //       if (!question.answeresRight) {
-  //         return setWrongAnswers(wrongAnswers+1)
-  //       }
-  //     })
-  // }, [result])
-
+  const countTime = (result) => {
+    const time_end = moment(result.time_end);
+    const time_start = moment(result.time_start);
+    setTime(time_end.diff(time_start, 'minutes'));
+  };
 
   return (
     <Container className={classes.mainContainer} component="main" maxWidth="lg">
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h3">
-          Testergebnisse
-        </Typography>
-
-        <Divider className={classes.divider} />
+      <Typography
+        component="h2"
+        variant="h4"
+        align="center"
+        className={classes.resultTitle}
+      >
+        Testergebnisse
+      </Typography>
+      <Divider className={classes.divider} />
+      {loading && (
+        <Box py={4} textAlign="center">
+          <CircularProgress color="primary" />
+        </Box>
+      )}
+      {result && (
         <Grid
           container
           spacing={3}
           alignItems="center"
           className={classes.padding}
         >
-          <Grid item xs={4}>
-            <ErrorOutline fontSize="large" />
-            <Typography>Fehler</Typography>
-            <Typography>{wrongAnswers && wrongAnswers}/{nrQuestions && nrQuestions}</Typography>
+          <Grid container item xs={6} alignItems="center" justify="center">
+            <Box
+              className={classes.resultBox}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              boxShadow={4}
+              borderRadius={4}
+            >
+              <CheckCircleOutlineOutlined fontSize="large" />
+              <Typography component="h4" variant="h6">
+                Fehler
+              </Typography>
+              <Typography className={classes.resultSummaryText}>
+                {rightAnswers}/{result.questions.length}
+              </Typography>
+            </Box>
           </Grid>
-
-          <Grid item xs={4}>
-            <AccessTime fontSize="large" />
-            <Typography>Benötigte Zeit</Typography>
-            <Typography>18.5min</Typography>
-          </Grid>
-
-          <Grid item xs={4}>
-            <StarOutline fontSize="large" />
-            <Typography>Bestzeit</Typography>
-            <Typography>15.5min</Typography>
+          <Grid container item xs={6} direction="column" alignItems="center">
+            <Box
+              className={classes.resultBox}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              boxShadow={4}
+              borderRadius={4}
+            >
+              <AccessTime fontSize="large" />
+              <Typography component="h4" variant="h6">
+                Benötigte Zeit
+              </Typography>
+              <Typography className={classes.resultSummaryText}>
+                {time}min
+              </Typography>
+            </Box>
           </Grid>
         </Grid>
-
-        {result &&
-          result.questions.map((res, index) => {
-            return (
-              <Accordion key={index} className={classes.accordion}>
-                <AccordionSummary
-                  className={
-                    res.answeresRight
-                      ? classes.right
-                      : classes.wrong
-                  }
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography>{res.question_text}</Typography>
-                </AccordionSummary>
-                {res.answers.map((ans, index) => {
-                  return (
-                    <AccordionDetails
-                      key={index}
-                      className={
-                        ans.checked === ans.userAnswer
-                          ? classes.right
-                          : classes.wrong
-                      }
-                    >
-                      <Typography>
-                        {ans.text}:{" "}
-                        {ans.checked === ans.userAnswer ? "Right" : "Wrong"}
-                      </Typography>
-                    </AccordionDetails>
-                  );
-                })}
-              </Accordion>
-            );
-          })}
-  <br />
-        <br />
+      )}
+      {result &&
+        result.questions.map((res, index) => {
+          return (
+            <Accordion key={index} className={classes.accordion}>
+              <AccordionSummary
+                className={classes.accordionIcon}
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                {res.answeresRight ? (
+                  <CheckCircle className={classes.right} />
+                ) : (
+                  <Error className={classes.wrong} />
+                )}
+                <Typography>{res.question_text}</Typography>
+              </AccordionSummary>
+              <AccordionDetails key={index}>
+                <Grid container>
+                  <Grid container item xs={1} justify="center">
+                    Your Answer
+                  </Grid>
+                  <Grid container item xs={1} justify="center">
+                    Answer
+                  </Grid>
+                  <Grid item xs={10}>
+                    Question
+                  </Grid>
+                </Grid>
+              </AccordionDetails>
+              {res.answers.map((ans, index) => {
+                return (
+                  <AccordionDetails key={index}>
+                    <Grid container>
+                      <Grid container item xs={1} justify="center">
+                        {ans.userAnswer ? (
+                          <FiberManualRecord className={classes.right} />
+                        ) : (
+                          <FiberManualRecord className={classes.wrong} />
+                        )}
+                      </Grid>
+                      <Grid container item xs={1} justify="center">
+                        {ans.checked ? (
+                          <FiberManualRecord className={classes.right} />
+                        ) : (
+                          <FiberManualRecord className={classes.wrong} />
+                        )}
+                      </Grid>
+                      <Grid item xs={10}>
+                        <Typography>{ans.text}</Typography>
+                      </Grid>
+                    </Grid>
+                  </AccordionDetails>
+                );
+              })}
+            </Accordion>
+          );
+        })}
+      <Box py={6} display="flex" justifyContent="center">
         <Link component={RouterLink} to="/dashboard">
           <Button variant="contained" color="primary">
             Zurück zum Dashboard
           </Button>
         </Link>
-
-        <br />
-        <br />
-        <br />
-      </div>
+      </Box>
     </Container>
   );
 };
 
-export default SelectCategory;
+export default Result;
 
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
-    backgroundColor: "rgba(255, 255, 255, 0.23)",
-    color: "#ffffff",
+    backgroundColor: 'rgba(255, 255, 255, 0.23)',
+    color: '#ffffff',
     borderRadius: 16,
-    border: "1px solid white",
-    borderColor: "primary",
+    border: '1px solid white',
+    borderColor: 'primary',
   },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: 10,
+  resultTitle: {
+    padding: '2rem',
   },
   padding: {
     padding: 50,
   },
+  resultBox: {
+    backgroundColor: '#232F37',
+    color: theme.palette.secondary.main,
+    width: '10rem',
+    height: '10rem',
+  },
+  resultSummaryText: {
+    color: '#fff',
+  },
   divider: {
-    backgroundColor: " #ffffff",
+    backgroundColor: ' #ffffff',
   },
   accordion: {
-    textAlign: "left",
-    width: "100%",
+    textAlign: 'left',
+    width: '100%',
+    backgroundColor: '#232F37',
+    color: '#fff',
+  },
+  accordionIcon: {
+    '& span': {
+      color: '#fff !important',
+    },
   },
   right: {
-    borderBottom: '1px solid #e6e6e6',
-    background: 'green',
+    margin: '0 1rem',
+    color: 'green',
   },
   wrong: {
-    borderBottom: '1px solid #e6e6e6',
-    background: 'red',
+    margin: '0 1rem',
+    color: 'red',
   },
 }));
